d796adfb5cf8c39e2d9ffc668dd33ff744a16718
Farid Ismailov <faridliners@gmail.com>
2019-03-06 19:42:33 +0300

    Add files via upload

---
+++ wel100assessment.js
@@ -0,0 +1,23 @@
+try {
+var widget = $('#assessmentWidget').data('els-elsAssessment'); 
+    var m = widget._model; 
+    m.setCurrentQuestion(0); 
+    for (var q = m.getQuestions(), ql = q.length, n = 0; ql > n; n++) { 
+        var a = q[n].correctAnswers.slice(); 
+        m.setCurrentQuestionAnswer(a); 
+        m.updateSessionQuestionAnswer(a); 
+        m.setNextQuestion(); 
+    } 
+    console.log('assessment score: %s%', m.getSession().getScore());
+} catch (e) {
+ var widget = $('#assessmentWidget').data('els-cpaAssessment'); 
+    var m = widget._model; 
+    m.setCurrentQuestion(0); 
+    for (var q = m.getQuestions(), ql = q.length, n = 0; ql > n; n++) { 
+        var a = q[n].correctAnswers.slice(); 
+        m.setCurrentQuestionAnswer(a); 
+        m.updateSessionQuestionAnswer(a); 
+        m.setNextQuestion(); 
+    } 
+    console.log('assessment score: %s%', m.getSession().getScore()); 
+}
