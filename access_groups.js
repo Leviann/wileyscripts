8be1bf537e014e99784f583f06fec6d43125150a
Farid Ismailov <faridliners@gmail.com>
2019-03-06 19:18:31 +0300

    Create access_groups.js

---
+++ access_groups.js
@@ -0,0 +1,156 @@
+GM_registerMenuCommand("2019 CPA Course", function(){
+    settings.accessGroupID = ["338426","338427","522337","338428","338429","338430","338432","522350","338431","338433","338434","338435","522351","338436","338437","338438","338439","522354","338440","338441","361142","361143","361144","361145"];
+    settings.linkForAccess = commonURLPart+"cpa/far.html";
+    init(settings);});
+
+GM_registerMenuCommand("2019 CFA Course", function(){
+    settings.accessGroupID = ["483570","483569","483571","483568","483572","483573","483574","483575","483576","483577","483578","483579","483580","483581","483582","483583","483584","483585","483586","483587","483588","483589","483590","483591","483592","483593","483594","483595","483596","483597","526640","526643","526644"];
+    settings.linkForAccess = commonURLPart+"cfa/level1_2019.html";
+    init(settings);});
+
+GM_registerMenuCommand("CFA 2019 11th Hour", function(){
+    settings.accessGroupID = ["606037","606043","606044","606040","483570","483569","606049","483571","606038","606045","606046","606041","483578","483579","605779","483580","606039","606047","606048","606042","483588","483589","606050","483590","483591","483592","483593","483594","483595","483596","483597","483581","483582","483583","483584","483585","483586","483587","483568","483572","483573","483574","483575","483576","483577","526640","526643","526644"];
+    settings.linkForAccess = commonURLPart+"cfa/level1_11th_2019.html";
+    init(settings);});
+
+GM_registerMenuCommand("2019 CIA Course", function(){
+    settings.accessGroupID = ["547595","547596","547597","547598","547599","547600","547601","547602","547603"];
+    settings.linkForAccess = commonURLPart+"cia/part1_2019.html";
+    init(settings);});
+
+GM_registerMenuCommand("2019 CMT Course", function(){
+    settings.accessGroupID = ["225633","225634","225636","225635","361161","361162","361163","604464","604465","604466","604467","604468","604469","604470","604471","604472","604473","604474","604475"];
+    settings.linkForAccess = commonURLPart+"cmt/level1.html";
+    init(settings);});
+
+GM_registerMenuCommand("2019 CMA Course", function(){
+    settings.accessGroupID = ["569325","569326","569330","569327","569328","569329","569331","569332","569333","569334","569335","569336"];
+    settings.linkForAccess = commonURLPart+"cma/part1_2019.html";
+    init(settings);});
+
+GM_registerMenuCommand("2019 GMAT Test Bank", function(){
+    settings.accessGroupID = ["427243","479961","550829","427242","427245","479962","427244","427247","479963","427246"];
+    settings.linkForAccess = commonURLPart+"gmat/gmat_2019.html";
+    init(settings);});
+
+GM_registerMenuCommand("2019 FINRA Course", function(){
+    settings.accessGroupID = ["604456","604457","604461","604458","604459","604460"];
+    settings.linkForAccess = commonURLPart+"finra/sie.html";
+    init(settings);});
+
+GM_registerMenuCommand("2019 CIMA Course", function(){
+    settings.accessGroupID = ["283033","283036","283034","283035","444453","444454","444457","444455","444456","361165"];
+    settings.linkForAccess = commonURLPart+"cima/cima.html";
+    init(settings);});
+
+GM_registerMenuCommand("2019 CFP Course", function(){
+    settings.accessGroupID = ["327358","327362","327363","327361","327360","361160"];
+    settings.linkForAccess = commonURLPart+"cfp/level1.html";
+    init(settings);});
+
+GM_registerMenuCommand("2019 ACT Course", function(){
+    settings.accessGroupID = ["427248","427249"];
+    settings.linkForAccess = commonURLPart+"act/act.html";
+    init(settings);});
+
+//---------------------------------------------------------------------------------------------------------------------------
+
+GM_registerMenuCommand("2018 CFA Course", function(){
+    settings.accessGroupID = ["361146","361147","361148","271436","421465","421469","421468","421470","282597","273566","271428","271439","271445","271434","271431","271442","271437","282598","273567","271429","271440","271446","271435","271432","271443","271438","282599","273568","271430","271441","271447","379016","271433","271444","271425","271426","271427"];
+    settings.linkForAccess = commonURLPart+"cfa/level1_2018.html";
+    init(settings);});
+
+GM_registerMenuCommand("2018 CMA Course", function(){
+    settings.accessGroupID = ["309989","309990","386236","386240","386237","535595","386238","386239","335938","335940","535600","335939","335941"];
+    settings.linkForAccess = commonURLPart+"cma/part1_2018.html";
+    init(settings);});
+
+GM_registerMenuCommand("2018 PMP Course", function(){
+    settings.accessGroupID = ["375004","375010","375005","375009","375007","375006","375008"];
+    settings.linkForAccess = commonURLPart+"pmp/pmp_2018.html";
+    init(settings);});
+
+GM_registerMenuCommand("2018 FRM Course", function(){
+    settings.accessGroupID = ["332489","332497","332491","332500","332499","332493","332490","332498","332492","332501","332494","576586","576591","361151","361152"];
+    settings.linkForAccess = commonURLPart+"frm/part1_2018.html";
+    init(settings);});
+
+GM_registerMenuCommand("2018 PMP Sybex", function(){
+    settings.accessGroupID = ["356906","378998","356905","356907"];
+    settings.linkForAccess = commonURLPart+"pmp/sybex.html";
+    init(settings);});
+
+GM_registerMenuCommand("2018 CAIA Course", function(){
+    settings.accessGroupID = ["304044","304050","361141","304047","304045","304046","504670","504671","504677","504673","504675","504676"];
+    settings.linkForAccess = commonURLPart+"caia/caia.html";
+    init(settings);});
+
+GM_registerMenuCommand("2018 CMT Test Bank with Export", function(){
+    settings.accessGroupID = ["225636","225635","225634","225633"];
+    settings.linkForAccess = commonURLPart+"cmt/level1.html";
+    init(settings);});
+
+GM_registerMenuCommand("2018 Security Course", function(){
+    settings.accessGroupID = ["474286","474288","490939","474290","525669","474289","474287"];
+    settings.linkForAccess = commonURLPart+"comptia/security.html";
+    init(settings);});
+
+GM_registerMenuCommand("2018 Security Study Test Bank", function(){
+    settings.accessGroupID = ["474263"];
+    settings.linkForAccess = commonURLPart+"comptia/security_study.html";
+    init(settings);});
+
+GM_registerMenuCommand("2018 Security Practice Test Bank", function(){
+    settings.accessGroupID = ["474265"];
+    settings.linkForAccess = commonURLPart+"comptia/security_practice.html";
+    init(settings);});
+
+GM_registerMenuCommand("2018 Security Deluxe Test Bank", function(){
+    settings.accessGroupID = ["474262"];
+    settings.linkForAccess = commonURLPart+"comptia/security_deluxe.html";
+    init(settings);});
+
+GM_registerMenuCommand("2018 Security Review Test Bank", function(){
+    settings.accessGroupID = ["474264"];
+    settings.linkForAccess = commonURLPart+"comptia/security_review.html";
+    init(settings);});
+
+GM_registerMenuCommand("2018 Investment Banking Course", function(){
+    settings.accessGroupID = ["567976","531289","531290","531291","518601","531292","418321","518594","518591","518598","170903","418324","518593","518592","518597","418325","518588","518595","518589","518599","518590","418322","518596","518587","518600","418323"];
+    settings.linkForAccess = commonURLPart+"ib/course1.html";
+    init(settings);});
+
+GM_registerMenuCommand("2018 Network+ Course", function(){
+    settings.accessGroupID = ["581658","596167","490933","490934","490938","490935","490936","490937","474286","474288","490939","474290","525669","474289","474287","581659","581660","581661","581662"];
+    settings.linkForAccess = commonURLPart+"comptia/network.html";
+    init(settings);});
+
+GM_registerMenuCommand("2018 Network+ Review Guide Test Bank", function(){
+    settings.accessGroupID = ["581661"];
+    settings.linkForAccess = commonURLPart+"comptia/network_review.html";
+    init(settings);});
+
+GM_registerMenuCommand("2018 Network+ Practice Tests Test Bank", function(){
+    settings.accessGroupID = ["581660"];
+    settings.linkForAccess = commonURLPart+"comptia/network_practice.html";
+    init(settings);});
+
+GM_registerMenuCommand("2018 Network+ Deluxe Study Guide Test Bank", function(){
+    settings.accessGroupID = ["581659"];
+    settings.linkForAccess = commonURLPart+"comptia/network_deluxe.html";
+    init(settings);});
+
+GM_registerMenuCommand("2018 Network+ Study Guide Test Bank", function(){
+    settings.accessGroupID = ["581662"];
+    settings.linkForAccess = commonURLPart+"comptia/network_study.html";
+    init(settings);});
+
+GM_registerMenuCommand("AWS PT Test Bank", function(){
+    settings.accessGroupID = ["604731"];
+    settings.linkForAccess = commonURLPart+"aws/pt.html";
+    init(settings);});
+
+GM_registerMenuCommand("AWS SG Test Bank", function(){
+    settings.accessGroupID = ["604732"];
+    settings.linkForAccess = commonURLPart+"aws/sg.html";
+    init(settings);});
